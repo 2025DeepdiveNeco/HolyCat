a256ef4f5fd7a02b4e3046313b228c1779bc78c4
Kimikhwan <105644387+Kimighwan@users.noreply.github.com>
2025-12-28 04:28:04 +0900

    feat : 결과창 꾸미기, 주수 버그 수정

--- Assets/02. Scripts/Effect/JucieWater.cs
+++ Assets/02. Scripts/Effect/JucieWater.cs
@@ -9,6 +9,7 @@ public class JucieWater : MonoBehaviour
         int index1 = Random.Range(0, 9);
         int index2 = Random.Range(0, 9);
         if (index1 == index2) index2++;
+        index2 %= 9;
 
         Instantiate(Resources.Load<GameObject>("Spike"), randomPos[index1].position, Quaternion.identity);
         Instantiate(Resources.Load<GameObject>("Spike"), randomPos[index2].position, Quaternion.identity);
